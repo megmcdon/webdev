3dd4b92e592083c9089c8cdcec9aa398981f8397
adelcayre <44105014+adelcayre@users.noreply.github.com>
2022-11-20 15:46:02 -0800

    Add files via upload

---
+++ simply/src/test/java/com/project/simply/SimplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.simply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
