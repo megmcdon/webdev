884fbc02bf876c30d29e6ff1396a5cda8dd004f6
megmcdon <meghanymcdonough@gmail.com>
2022-12-08 23:09:58 -0500

    added ssl
    use port https://localhost:8443

--- simply/src/main/java/com/project/security/SecurityConfig.java
+++ simply/src/main/java/com/project/security/SecurityConfig.java
@@ -2,10 +2,21 @@ package com.project.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig {
 
+  @Bean
+  DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http
+      .requiresChannel(channel -> 
+          channel.anyRequest().requiresSecure())
+      .authorizeRequests(authorize ->
+          authorize.anyRequest().permitAll())
+      .build();
+    }
 
 }
